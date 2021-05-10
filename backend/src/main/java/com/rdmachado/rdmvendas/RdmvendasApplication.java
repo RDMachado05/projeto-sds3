12e152405e2b1eea210eda28f3f8296a0a64aaec
Rodrigo Duarte Machado <rdmachado05@gmail.com>
2021-05-10 00:02:06 -0300

    Project created

---
+++ backend/src/main/java/com/rdmachado/rdmvendas/RdmvendasApplication.java
@@ -0,0 +1,13 @@
+package com.rdmachado.rdmvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdmvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdmvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rdmachado/rdmvendas/RdmvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdmachado.rdmvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdmvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
